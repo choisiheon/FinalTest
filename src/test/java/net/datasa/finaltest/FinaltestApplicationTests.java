b86ea36fd12c9f6cf18eaddc949d2d5e3e5f3418
choisiheon <0328ks@gmail.com>
2026-01-15 09:12:19 +0900

    fc

---
+++ src/test/java/net/datasa/finaltest/FinaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.datasa.finaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
